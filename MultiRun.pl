cc3f6d37e81ae60551cc18e91fe5bf7f9a2bc041
Csaba Kerepesi <kerepesics@gmail.com>
2017-02-09 22:09:31 +0100

    Update MultiRun.pl

--- MultiRun.pl
+++ MultiRun.pl
@@ -5,7 +5,7 @@ use warnings;
 open IN, '>', "randomnumbers.txt";
 for (my $i=1;$i<=500;$i++) {
 	my $rnd=int(rand(20000));
-	system ("immune3_dec17_2014 $rnd");
+	system ("MiStImm_dec17_2014 $rnd");
 	print IN "$rnd\n";
 }
 close IN;
