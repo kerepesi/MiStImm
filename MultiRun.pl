43eb3013187d8081c2409ebd88fc3ff2589f765e
Csaba Kerepesi <kerepesics@gmail.com>
2017-10-18 13:21:03 +0200

    Add files via upload

--- MultiRun.pl
+++ MultiRun.pl
@@ -3,9 +3,9 @@ use strict;
 use warnings;
 
 open IN, '>', "randomnumbers.txt";
-for (my $i=1;$i<=500;$i++) {
+for (my $i=1;$i<=20;$i++) {
 	my $rnd=int(rand(20000));
-	system ("MiStImm_dec17_2014 $rnd");
+	system ("MiStImm_oct18_2017 $rnd");
 	print IN "$rnd\n";
 }
 close IN;
